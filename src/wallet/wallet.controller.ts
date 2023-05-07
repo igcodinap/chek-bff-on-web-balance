47a7521f547ffa4fb71a24a3d4a2643c84896729
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-07 19:51:03 -0400

    typo

--- src/wallet/wallet.controller.ts
+++ src/wallet/wallet.controller.ts
@@ -1,30 +1,33 @@
-import { Controller, Get, HttpException, HttpStatus, Request } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Request,
+} from '@nestjs/common';
 import { WalletService } from './wallet.service';
 
 @Controller('wallet')
 export class WalletController {
-    walletService: WalletService;
-    constructor(service: WalletService) {
-        this.walletService = service;
-    }
+  walletService: WalletService;
+  constructor(service: WalletService) {
+    this.walletService = service;
+  }
 
-    @Get('balance')
-    async getUserBalance(@Request() req) {
-        try {
-            const userid = '1';
-            const response = await this.walletService.getBalanceById(userid);
-            return response;
-        } catch (error) {
-            throw new HttpException(
-                {
-                    status: HttpStatus.BAD_REQUEST,
-                    error: error.message,
-                },
-                HttpStatus.BAD_REQUEST,
-            );
-        }
-
+  @Get('balance')
+  async getUserBalance(@Request() req) {
+    try {
+      const userid = '1';
+      const response = await this.walletService.getBalanceById(userid);
+      return response;
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: error.message,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
     }
-
-    }
+  }
 }
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WalletService } from './wallet.service';
 import { WalletController } from './wallet.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [WalletService],
-  controllers: [WalletController]
+  controllers: [WalletController],
 })
 export class WalletModule {}
