c83cbd59aae1e3ba30376049261605604cfe4f88
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-07 19:11:58 -0400

    working on auth controller login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+  Request,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  authService: AuthService;
+  constructor(service: AuthService) {
+    this.authService = service;
+  }
+
+  @Post('login')
+  async login(@Request() req) {
+    try {
+      const { email, password } = req.body;
+      console.log(email, password);
+      const reponse = await this.authService.login(email, password);
+      console.log(reponse, 'reponse');
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: error.message,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
