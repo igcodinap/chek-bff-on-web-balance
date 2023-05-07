67daa3f8e457e85cc93031e2771e97de8457f450
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-07 19:44:46 -0400

    wallet service wip

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable()
-export class WalletService {}
+export class WalletService {
+  httpService: HttpService;
+  constructor(service: HttpService) {
+    this.httpService = service;
+  }
+
+  async getBalanceById(id: string) {
+    const response = await firstValueFrom(
+      this.httpService.get(`http://localhost:3002/wallet/balance/${id}`),
+    );
+    console.log(response, 'response');
+    return response.data;
+  }
+}
