1a094a7c01729635f929c27f7f91de97bf8cb1a3
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-11 19:01:53 -0400

    cleaning up

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { WalletModule } from './wallet/wallet.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [AuthModule, WalletModule, ConfigModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
