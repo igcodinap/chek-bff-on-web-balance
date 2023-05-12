c18425771c641558c4df227cfe71e17cb0bc08ed
Ignacio Codina <ext_iandcodinap@falabella.cl>
2023-05-12 09:41:34 -0400

    working on tests auth module wip

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { HttpException, HttpStatus } from '@nestjs/common';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
+    const mockAuthService = {
+      login: jest.fn().mockResolvedValue('any'),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should call authService.login with correct information from req.body', async () => {
+    const req = {
+      body: {
+        email: 'user@mail.com',
+        password: '123456',
+      },
+    };
+    await authController.login(req);
+    expect(authService.login).toHaveBeenCalledWith('user@mail.com', '123456');
+  });
+
+  it('should throw an http error when login fails', async () => {
+    const req = {
+      body: {
+        email: 'user@mail.com',
+        password: 'wrongpassword',
+      },
+    };
+    const errorMessage = 'Login failed';
+    const loginError = new Error(errorMessage);
+    (authService.login as jest.Mock).mockRejectedValue(loginError);
+    await expect(authController.login(req)).rejects.toThrow(
+      new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: errorMessage,
+        },
+        HttpStatus.BAD_REQUEST,
+      ),
+    );
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { HttpService } from '@nestjs/axios';
+import { of } from 'rxjs';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let httpService: HttpService;
 
   beforeEach(async () => {
+    const mockHttpService = {
+      post: jest.fn().mockImplementation(() => of({ data: 'any' })),
+    };
+
+    const mockConfigService = {
+      get: jest.fn().mockReturnValue('http://localhost:3002'),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: HttpService, useValue: mockHttpService },
+        { provide: ConfigService, useValue: mockConfigService },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    httpService = module.get<HttpService>(HttpService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should call httpService.post with correct parameters', async () => {
+    const email = 'user@mail.com';
+    const password = '123456';
+    await authService.login(email, password);
+    expect(httpService.post).toHaveBeenCalledWith(
+      'http://localhost:3002/auth/login',
+      { email, password },
+    );
+  });
+
+  it('should return the response data', async () => {
+    const email = 'user@mail.com';
+    const password = '123456';
+    const response = await authService.login(email, password);
+    expect(response).toEqual('any');
   });
 });
